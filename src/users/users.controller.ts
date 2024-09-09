8ca04161ee72d1b13d1ba9da03dad1937c1f8540
pmin <mhp4004@gmail.com>
2024-09-10 00:13:30 +0900

    post 테스트 작성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Post, Patch, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
 
-    @Get()
-    public getUsers(){
+    //:id?는 선택이다 없어도 접근가능, :id는 필수로 필요하다
+    @Get('/:id/:optional?')
+    public getUsers(@Param() params: any, @Query() query: any){
+        console.log(params);
+        console.log(query);
         return "You sent a get request to users endpoint";
     }
 
     @Post()
-    public createUsers(){
+    public createUsers(@Body() request: any){
+        console.log(request);
         return "You sent a post request to users endpoint";
     }
 

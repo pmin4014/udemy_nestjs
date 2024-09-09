44a54b1bd15590759d5d1f3788e0938b0d8c687a
pmin <mhp4004@gmail.com>
2024-09-10 00:21:10 +0900

    들어오는 request에서 원하는 정보를 가져오는법

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,25 @@
-import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body, Headers, Head, Ip } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
 
     //:id?는 선택이다 없어도 접근가능, :id는 필수로 필요하다
     @Get('/:id/:optional?')
-    public getUsers(@Param() params: any, @Query() query: any){
-        console.log(params);
-        console.log(query);
+    public getUsers(@Param('id') id: any, @Query('limit') limit: any){
+        console.log(id);
+        console.log(limit);
         return "You sent a get request to users endpoint";
     }
 
     @Post()
-    public createUsers(@Body() request: any){
+    public createUsers(
+        @Body() request: any,
+        @Headers() headers: any,
+        @Ip() ip: any,
+    ){
         console.log(request);
+        console.log(headers);
+        console.log(ip);
         return "You sent a post request to users endpoint";
     }
 

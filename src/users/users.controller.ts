40de4eb9ba25376c1beb6cb3d3111ffa18227129
pmin <mhp4004@gmail.com>
2024-09-11 19:46:48 +0900

    23, DefaultValuePipe 사용, 쿼리가 입력되지 않아도 기본값을 설정하여 적용한다.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body, Headers, Head, Ip, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body, Headers, Head, Ip, ParseIntPipe, DefaultValuePipe } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -6,10 +6,10 @@ export class UsersController {
     //:id?는 선택이다 없어도 접근가능, :id는 필수로 필요하다
     @Get('/:id?')
     public getUsers(@Param('id', ParseIntPipe) id: number | undefined,
-    @Query('limit', ParseIntPipe) limit: any,
-    @Query('page', ParseIntPipe) page: any){
-        console.log(typeof id);
-        console.log(id);
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number){
+        console.log(limit);
+        console.log(page);
         return "You sent a get request to users endpoint";
     }
 

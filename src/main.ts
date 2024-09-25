0631ca3083d924f7300384f8aa5c613984221755
pmin <mhp4004@gmail.com>
2024-09-25 21:08:16 +0900

    main.ts에 글로벌 파이프를 연결해서 각 컨트롤러에서 파이프를 연결하지 않아도 작동한다.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //main.ts에 글로벌 파이프를 사용함으로써 컨트롤러에서 따로 파이프를 연결하지 않아도 된다.
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
 bootstrap();

f743b3782d32be2abc10810ab5d4c86169762ab1
pmin <mhp4004@gmail.com>
2024-10-14 21:44:40 +0900

    49. swagger을 사용하여 GET문서 꾸미기

--- src/users/dtos/get-users-param.dto.ts
+++ src/users/dtos/get-users-param.dto.ts
@@ -1,7 +1,12 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsInt, IsOptional } from "class-validator";
 
 export class GetUsersParamDto{
+    @ApiPropertyOptional({
+        description:"특정 ID를 가진 사용자를 가져온다.",
+        example: 1234,
+    })
     @IsOptional()
     @IsInt()
     //Http 요청의 쿼리는 기본적으로 문자열이라서 Type을 사용해서 Number로 변환해준다
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
 import { UsersService } from './providers/users.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
 @ApiTags('사용자 Users')
@@ -15,6 +15,27 @@ export class UsersController {
 
     //:id?는 선택이다 없어도 접근가능, :id는 필수로 필요하다
     @Get('/:id?')
+    @ApiOperation({
+        summary: '등록된 사용자 목록을 가져온다.',
+    })
+    @ApiResponse({
+        status: 200,
+        description: '성공적으로 가져왔다.'
+    })
+    @ApiQuery({
+        name: 'limit',
+        type: 'number',
+        required: false,
+        description: '쿼리당 반환되는 항목 수',
+        example: '10'
+    })
+    @ApiQuery({
+        name: 'page',
+        type: 'number',
+        required: false,
+        description: '반환될 페이지번호의 위치',
+        example: '1'
+    })
     public getUsers(
     @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
